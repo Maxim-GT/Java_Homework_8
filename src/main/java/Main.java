c4df1920faad64dec21a724db6828951290cf797
Maxim Glukhov <glukhov_maxim@mail.ru>
2022-08-10 19:54:25 +0700

    Main files changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Post post = new Post();
         post.birthday = new FormDate();
-        post.birthday.day = String.valueOf(13);
-        post.birthday.month = String.valueOf(6);
-        post.birthday.year = String.valueOf(1999);
+        post.birthday.day = Integer.parseInt(String.valueOf(13));
+        post.birthday.month = Integer.parseInt(String.valueOf(6));
+        post.birthday.year = Integer.parseInt(String.valueOf(1999));
         post.name = "Иван";
         post.passport = "4444 № 44444444";
         post.phone = "+7 (999)-999-99-99";
